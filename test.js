75b4fe39e37258ef1dc0a271d8c12fe6a6d43206
taner <taner.diler@gmail.com>
2017-12-12 06:52:48 +0300

    Allows to specify method name diff than eventname
    On 1.0.1 version, event name was being used as method name to trigger listeners.
    
    Now, you can specify a different name as default method name. If you don't specify,
    event name will be used as default name by putting on prefix when needed.
    
    Updates README.md
    
    Version 1.0.2

--- test.js
+++ test.js
@@ -5,12 +5,6 @@ chai.should();
 var eventbus = require('./eventbus');
 describe('Eventbus lets you execute your business logic by providing sequential event/listener approach',function(){
 
-    it("should put 'on' prefix if name doesn't have",function(){
-        var event = eventbus.event("MouseClick");
-        event.name().should.equal("onMouseClick");
-    })
-
-
     it('should create an event once',function(){
         var event_1 = eventbus.event('onMouseClick');
         var event_2 = eventbus.event('onMouseClick');
@@ -18,42 +12,64 @@ describe('Eventbus lets you execute your business logic by providing sequential 
     })
 
     it('should call listener if no condition specified',function(){
+
         var listenerCallingTimes = 0;
-        eventbus.listener({onMouseClick:function () {listenerCallingTimes++;}}).listen('onMouseClick');
-        eventbus.event('MouseClick').fire();
+        eventbus.event('MouseOver')
+        eventbus.listener({onMouseOver:function () {listenerCallingTimes++;}}).listen('MouseOver');
+        eventbus.event('MouseOver').fire();
+
         listenerCallingTimes.should.equal(1);
     })
 
     it('should call two listeners if no condition specified',function(){
         var listenerCallingTimes = 0;
-        eventbus.listener({onMouseClick:function () {listenerCallingTimes++;}}).listen('onMouseClick');
-        eventbus.listener({onMouseClick:function () {listenerCallingTimes = listenerCallingTimes + 2;}}).listen('onMouseClick');
-        eventbus.event('MouseClick').fire();
+        eventbus.event('MouseOut');
+        eventbus.listener({onMouseOut:function () {listenerCallingTimes++;}}).listen('MouseOut');
+        eventbus.listener({onMouseOut:function () {listenerCallingTimes = listenerCallingTimes + 2;}}).listen('MouseOut');
+        eventbus.event('MouseOut').fire();
         listenerCallingTimes.should.equal(3);
     })
 
     it('should call listener once with condition',function(){
         var listenerCallingTimes = 0;
+        eventbus.event('KeyDown');
         var listener = eventbus.listener(
-            {onMouseClick: function () {
+            {onKeyDown: function () {
                 listenerCallingTimes++;
             }
             })
-            .listen('onMouseClick')
+            .listen('KeyDown')
             .when(
                 function(source){
                     return listenerCallingTimes < 1;
                 });
-        eventbus.event('MouseClick').fire();
-        eventbus.event('MouseClick').fire();
+        eventbus.event('KeyDown').fire();
+        eventbus.event('KeyDown').fire();
         listenerCallingTimes.should.equal(1);
     })
 
     it('should stop triggering next listeners', function(){
         var listenerCallingTimes = 0;
-        eventbus.listener({onMouseClick:function () {listenerCallingTimes++; return false;}}).listen('onMouseClick');
-        eventbus.listener({onMouseClick:function () {listenerCallingTimes = listenerCallingTimes + 2; return false;}}).listen('onMouseClick');
-        eventbus.event('MouseClick').fire();
+        eventbus.event('KeyUp');
+        eventbus.listener({onKeyUp:function () {listenerCallingTimes++; return false;}}).listen('KeyUp');
+        eventbus.listener({onKeyUp:function () {listenerCallingTimes = listenerCallingTimes + 2; return false;}}).listen('KeyUp');
+        eventbus.event('KeyUp').fire();
+        listenerCallingTimes.should.equal(1);
+    })
+
+    it('event should be named different than method name', function(){
+        var listenerCallingTimes = 0;
+        eventbus.event('BadWordDetection').setDefaultMethod('aBadWordDetected');
+        eventbus.listener({aBadWordDetected:function(source){listenerCallingTimes++;}}).listen('BadWordDetection');
+        eventbus.event('BadWordDetection').fire({badword:'nothing'});
+        listenerCallingTimes.should.equal(1);
+    })
+
+    it('event name should be assigned to method name with putting on prefix if needed, by default ', function(){
+        var listenerCallingTimes = 0;
+        eventbus.event('BadWordRemoved');
+        eventbus.listener({onBadWordRemoved:function(source){listenerCallingTimes++;}}).listen('BadWordRemoved');
+        eventbus.event('BadWordRemoved').fire({badword:'nothing'});
         listenerCallingTimes.should.equal(1);
     })
 
